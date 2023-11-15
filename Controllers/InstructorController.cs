e85523d3cf133404a9f2196c727c89a32d72f009
Emirhan <emirhannkilicaslann@gmail.com>
2023-11-15 11:26:57 +0300

    Edit and Delete functionality added for Instructor

--- Controllers/InstructorController.cs
+++ Controllers/InstructorController.cs
@@ -27,5 +27,72 @@ namespace CourseApp.Data
             await _context.SaveChangesAsync();
             return RedirectToAction("Index");
         }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var instructor = await _context.Instructors.FirstOrDefaultAsync(i => i.InstructorId == id);
+            if(instructor == null)
+            {
+                return NotFound();
+            }
+            return View(instructor);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Instructor instructor, int id)
+        {
+            if (id != instructor.InstructorId)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(instructor);
+                    await _context.SaveChangesAsync();
+                }
+                catch(DbUpdateConcurrencyException)
+                {
+                    if(!_context.Instructors.Any(ins => ins.InstructorId == instructor.InstructorId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(instructor);
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var instructor = await _context.Instructors.FindAsync(id);
+            if (instructor == null)
+            {
+                return NotFound();
+            }
+            return View(instructor);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete([FromForm] int id)
+        {
+            var instructor = await _context.Instructors.FindAsync(id);
+            if(instructor == null)
+            {
+                return NotFound();
+            }
+            _context.Instructors.Remove(instructor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
     }
 }
